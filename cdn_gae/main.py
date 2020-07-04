74fcaad2324961b68274df8c3abf249a8f03c6cd
Alex Derbes <acd@263.lan>
2020-07-04 16:57:19 -0700

    added stuff

--- cdn_gae/main.py
+++ cdn_gae/main.py
@@ -21,17 +21,17 @@ import sys
 from urllib.parse import urlparse
 from flask import render_template
 from google.cloud import storage
-import hashlib
 import time
+import argparse
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
 
 from haralyzer import HarParser, HarPage
 
-h_md5 = hashlib.md5()
-
-hyperlink_format = '<a href="{link}">{text}</a>'
-
-
 def linkify(url):
+    """takes a URL and returns the HTML for a link to the URL"""
+    # url = "url to make into an HTML link"
+    hyperlink_format = '<a href="{link}">{text}</a>'
     return hyperlink_format.format(link=url, text=url)
     
 def upload_blob(bucket_name, stuff, destination_blob_name):
@@ -43,40 +43,16 @@ def upload_blob(bucket_name, stuff, destination_blob_name):
     storage_client = storage.Client()
     bucket = storage_client.bucket(bucket_name)
     blob = bucket.blob(destination_blob_name)
-
     blob.upload_from_string(stuff)
-
-    print(
-        "Bucket {} uploaded to {}.".format(
-            bucket_name, destination_blob_name
-        )
-    )
-
-# If `entrypoint` is not defined in app.yaml, App Engine will look for an app
-# called `app` in `main.py`.
-app = Flask(__name__)
-
-@app.route('/')
-def write_form():
-    """Return a friendly HTTP greeting."""
-    return render_template('form.html', title='Home')
     
-@app.route('/upload_file', methods=['POST'])
-def upload_file():
-    f = request.files['yourFileName'].read()
-    try:
-        ts = time.time()
-        upload_blob('cdninfolyzer.appspot.com', f, "data/" + str(ts))
-    except Exception as e:
-        print (e)
-        
+def parse_file(f):
     har_parser = HarParser(json.loads(f))
 
     rows = [['X-CACHE-HEADER', 'BYTES', 'URL']]
 
     hosts = {}
     size = {}
-    total_bytes = 0
+    total_bytes = 0 #total bytes for all content across the entire thing 
 
     for page in har_parser.pages:
         assert isinstance(page, HarPage)
@@ -86,22 +62,21 @@ def upload_file():
             #print(entry['response'], file=sys.stderr)
             cdn_str = None
             total_bytes += entry['response']['content']['size']
+            #pp.pprint(entry['request'])
+            url = urlparse(entry['request']['url'])
             for h in headers: 
                 if( h['name'] == 'x-cache'):
-                    url = urlparse(entry['request']['url'])
                     hosts[url.netloc] = 1
                     #print(url, file=sys.stderr)
                     cdn_str = h['value']
                     cdn.append(cdn_str)
+            
             if( cdn_str in size ):
                 size[cdn_str] = size[cdn_str] + entry['response']['content']['size']
             else:
                 size[cdn_str] = entry['response']['content']['size']
-        
+            print("\t".join([str(cdn), str(entry['response']['content']['size']), entry['request']['url'], url.netloc]))
             rows.append([cdn, entry['response']['content']['size'], linkify(entry['request']['url'])])
-    print (hosts)
-    print (size)
-    print (total_bytes)
     
     bysize = [['CACHE TAG', '% OF BYTES']]
     for sk in size.keys():
@@ -109,12 +84,77 @@ def upload_file():
         
         bysize_t = list(map(list, zip(*bysize))) 
         hosts_t = list(map(list, zip(*[hosts.keys()]))) 
-    return render_template('results.html', titles=['', 'Total bytes', 'Hosts', 'By Service', 'Results' ], tables=[[['Total bytes:', total_bytes]],hosts_t, bysize, rows])
+    return {'total_bytes':total_bytes, 'hosts_t':hosts_t, 'bysize':bysize, 'rows':rows}    
     #return json.dumps([hosts, size, rows])
+    
+
+# If `entrypoint` is not defined in app.yaml, App Engine will look for an app
+# called `app` in `main.py`.
+app = Flask(__name__)
+
+@app.route('/')
+def write_form():
+    """Render landing page with instructions and form to upload an HAR file"""
+    return render_template('form.html', title='Home')
+    
+@app.route('/upload_file', methods=['POST'])
+def upload_file():
+    """Process the uploaded file. Save a copy to a cloud bucket. Then parse out the headers and grab the x-cache headers that show which CDN it loaded from"""
+    f = request.files['yourFileName'].read()
+    try:
+        ts = time.time()
+        upload_blob('cdninfolyzer.appspot.com', f, "data/" + str(ts))
+    except Exception as e:
+        print (e)
+    r = parse_file(f)
+    return render_template('results.html', titles=['', 'Total bytes', 'Hosts', 'By Service', 'Results' ], tables=[[['Total bytes:', r['total_bytes']]],r['hosts_t'], r['bysize'], r['rows']])
+    
+@app.route('/list_files')
+def do_list_files():
+    """Lists all buckets."""
+
+
+    """Lists all the blobs in the bucket."""
+    # bucket_name = "your-bucket-name"
+
+    storage_client = storage.Client()
+
+    # Note: Client.list_blobs requires at least package version 1.17.0.
+    blobs = storage_client.list_blobs("cdninfolyzer.appspot.com" ,prefix="data/")
+
+    blist = []
+
+    for blob in blobs:
+        blist.append(blob.name)
+
+    return str(blist)
+    
+@app.route('/results')
+def do_results():
+    """display static results page"""
+    return render_template('results.html', title='Home')
 
 if __name__ == '__main__':
+    
+    parser = argparse.ArgumentParser(description='process session files')
+    parser.add_argument('--bulk', nargs='+', type=open, help='upload all files matching to database')
+    args = parser.parse_args()
+
+    
+    if args.bulk:
+        print ("bulk")
+        
+        for a in args.bulk:
+        
+            #print ( parse_file (a.read())['bysize'] , file=sys.io.stderr)
+            try: 
+                print ( parse_file (a.read())['bysize'] , file=sys.io.stderr)
+            except Exception as e:
+                print (e)
+            
     # This is used when running locally only. When deploying to Google App
     # Engine, a webserver process such as Gunicorn will serve the app. This
     # can be configured by adding an `entrypoint` to app.yaml.
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    else:
+        app.run(host='127.0.0.1', port=8080, debug=True)
 # [END gae_python38_app]
